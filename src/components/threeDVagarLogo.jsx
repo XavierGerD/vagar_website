0b2113c2ccfc3ba67756c32b2633873ffb44f71e
XavierGerD <xaviergervais@gmail.com>
2020-03-24 18:51:43 -0400

    Animation cleanup

--- src/components/threeDVagarLogo.jsx
+++ src/components/threeDVagarLogo.jsx
@@ -31,13 +31,111 @@ let logo;
 let textBoxes;
 let cloudParticles = [];
 let tweensPosition = [];
-let tweensRotation = [];
 let tweenLogo;
 let tweenLogoPosition;
-let isMoving = false;
-let animationRunning = false;
 let boxSize = 0;
 let otherBox = true;
+
+let tweens = {
+	boxPositions: [],
+	boxRotations: [],
+	animationRunning: false,
+	isMoving: false,
+	setupLogoAnim: () => {
+		tweens.logoExit
+			.to({ y: 2.2 }, 500)
+			.easing(TWEEN.Easing.Quartic.Out)
+			.onStart(() => (tweens.animationRunning = true));
+
+		tweens.logoEnter
+			.to({ y: 1.2 }, 1000)
+			.easing(TWEEN.Easing.Quartic.Out)
+			.delay(500)
+			.onComplete(() => (tweens.animationRunning = false));
+
+		tweens.logoFirstEnter
+			.to({ y: 1.2 }, 1000)
+			.easing(TWEEN.Easing.Quartic.Out)
+			.delay(1000)
+			.onComplete(() => (tweens.animationRunning = false));
+	},
+
+	contentEntranceSetup: () => {
+		tweens.boxPositions.forEach(tween => {
+			tween
+				.to({ y: "+7" }, 1000)
+				.delay(1000)
+				.easing(TWEEN.Easing.Quartic.Out);
+		});
+	},
+
+	contentEntranceAnimation: () => {
+		tweens.boxPositions.forEach(tween => {
+			tween.start();
+		});
+		tweens.resetTweens();
+	},
+
+	contentExitSetup: () => {
+		tweens.boxPositions.forEach(tween => {
+			tween.to({ y: "+30" }, 1000).easing(TWEEN.Easing.Quartic.Out);
+		});
+	},
+
+	contentExitAnimation: () => {
+		tweens.boxPositions.forEach(tween => {
+			tween.start();
+		});
+		tweens.resetTweens();
+	},
+
+	mediaEntranceSetup: () => {
+		tweens.boxPositions.forEach((tween, i) => {
+			const isEven = i % 2 === 0 ? -1 : 1;
+			const x = isEven * 3;
+			tween
+				.to({ x }, 1000)
+				.easing(TWEEN.Easing.Quartic.Out)
+				.delay(1000);
+		});
+	},
+
+	mediaEntranceAnimation: () => {
+		tweens.boxPositions.forEach(tween => {
+			tween.start();
+		});
+	},
+
+	mediaExitSetup: () => {
+		tweens.boxPositions.forEach((tween, i) => {
+			const isEven = i % 2 === 0 ? -1 : 1;
+			const x = isEven * 15;
+			tween.to({ x }, 1000).easing(TWEEN.Easing.Quartic.Out);
+		});
+	},
+
+	mediaExitAnimation: () => {
+		tweens.boxPositions.forEach(tween => {
+			tween.start();
+		});
+	},
+
+	resetTweens: () => {
+		tweens.boxPositions = [];
+		tweens.boxRotations = [];
+		textBoxes.forEach(textBox => {
+			tweens.boxPositions.push(new TWEEN.Tween(textBox.position));
+			tweens.boxRotations.push(new TWEEN.Tween(textBox.rotation));
+		});
+	},
+
+	resetLogo: () => {
+		tweens.logoEnter = new TWEEN.Tween(logo.position);
+		tweens.logoExit = new TWEEN.Tween(logo.position);
+		tweens.logoFirstEnter = new TWEEN.Tween(logo.position);
+		tweens.setupLogoAnim();
+	}
+};
 
 const maxAnisotropy = renderer.capabilities.getMaxAnisotropy();
 
@@ -52,84 +150,95 @@ export default class ThreeDeeLogo extends Component {
 		window.addEventListener("click", this.handleClick);
 		directionalLight.position.set(0, 0, 1);
 		renderer.setSize(window.innerWidth, window.innerHeight);
+		const root = document.getElementById("navbar");
 		renderer.id = "canvas";
-		document.body.appendChild(renderer.domElement);
+		document.body.insertBefore(renderer.domElement, root);
 		this.loadLogo()
+			.then(() => this.setup())
 			.then(() => this.loadNews())
 			.then(() => loadClouds(scene, cloudParticles))
-			.then(() => this.setup())
 			.then(() => {
-				tweenLogoPosition = new TWEEN.Tween(logo.position);
-				tweenLogo = new TWEEN.Tween(logo.rotation);
+				tweens.resetLogo();
 			})
 			.then(() => this.animate())
-			.then(() => this.contentEntranceAnimation());
+			.then(() => {
+				tweens.contentEntranceAnimation();
+				tweens.logoFirstEnter.start();
+				tweens.resetLogo();
+			});
 		scene.add(ambientLight);
 		scene.add(directionalLight);
 		camera.position.z = 2;
 	};
 
 	handleClick = () => {
-		if (animationRunning) return;
+		if (tweens.animationRunning) return;
 		if (otherBox) {
-			this.contentExitAnimation();
+			tweens.logoExit.chain(tweens.logoEnter);
+			tweens.logoExit.start();
+			tweens.resetLogo();
+			tweens.contentExitSetup();
+			tweens.contentExitAnimation();
 			this.loadMedia();
-			this.mediaEntranceAnimation();
+			tweens.mediaEntranceSetup();
+			tweens.mediaEntranceAnimation();
+			tweens.resetTweens();
 			otherBox = false;
 			return;
 		}
-		this.mediaExitAnimation();
+		tweens.logoExit.chain(tweens.logoEnter);
+		tweens.logoExit.start();
+		tweens.resetLogo();
+		tweens.mediaExitSetup();
+		tweens.mediaExitAnimation();
 		this.loadNews();
-		this.contentEntranceAnimation();
+		tweens.contentEntranceSetup();
+		tweens.contentEntranceAnimation();
 		otherBox = true;
 	};
 
 	handleArrows = event => {
 		let sign = 1;
 		if (event.deltaY < 0) sign = -1;
-		console.log(textBoxes);
+
 		const isFirstElemAtZero = textBoxes[0].position.y > 0;
 		const isLastElemAtZero = textBoxes[textBoxes.length - 1].position.y < 0;
 		if (
 			(!isFirstElemAtZero && sign === 1) ||
 			(!isLastElemAtZero && sign === -1) ||
-			isMoving ||
-			animationRunning
+			tweens.isMoving ||
+			tweens.animationRunning
 		) {
 			return;
 		}
-		isMoving = true;
-		tweensPosition.forEach((tween, i) => {
+		tweens.isMoving = true;
+		tweens.boxPositions.forEach((tween, i) => {
 			const y = tween._object.y + -boxSize * sign;
 			const z = tween._object.z - 1 * sign;
 			tween
 				.to({ y, z }, 300)
 				.start()
-				.onComplete(() => (isMoving = false));
+				.onComplete(() => (tweens.isMoving = false));
 
-			const rotationX = tweensRotation[i]._object.x + 0.2 * sign;
+			const rotationX = tweens.boxRotations[i]._object.x + 0.2 * sign;
 			const rotationDirection = i % 2 === 0 ? 1 : -1;
 			const rotationY =
-				tweensRotation[i]._object.y + rotationDirection * sign * 0.05;
-			tweensRotation[i].to({ x: rotationX, y: rotationY }, 300).start();
-
-			const logoPosY = tweenLogoPosition._object.y + -boxSize * sign;
-			tweenLogoPosition.to({ y: logoPosY }, 300).start();
+				tweens.boxRotations[i]._object.y +
+				rotationDirection * sign * 0.05;
+			tweens.boxRotations[i]
+				.to({ x: rotationX, y: rotationY }, 300)
+				.start();
 		});
+		const moveLogo = new TWEEN.Tween(logo.position);
+		const logoPosY = logo.position.y + -boxSize * sign;
+		moveLogo.to({ y: logoPosY }, 300).start();
 
-		tweensPosition = [];
-		tweensRotation = [];
-		textBoxes.forEach(textBox => {
-			tweensPosition.push(new TWEEN.Tween(textBox.position));
-			tweensRotation.push(new TWEEN.Tween(textBox.rotation));
-		});
-		tweenLogoPosition = new TWEEN.Tween(logo.position);
+		tweens.resetTweens();
+		tweens.resetLogo();
 	};
 
 	loadNews = () => {
 		textBoxes = [];
-		tweensPosition = [];
-		tweensRotation = [];
 		boxSize = 4.5;
 		textBoxes = createContentBlocks(content, maxAnisotropy, 1100, 375);
 
@@ -139,16 +248,14 @@ export default class ThreeDeeLogo extends Component {
 			textBox.translateY(i * -4.5 - 7);
 			textBox.rotateX(i * 0.2);
 			textBox.rotateY((i % 2 === 0 ? 0.05 : -0.05) * i);
-
-			tweensPosition.push(new TWEEN.Tween(textBox.position));
-			tweensRotation.push(new TWEEN.Tween(textBox.rotation));
 		});
+		tweens.resetTweens();
+		tweens.resetLogo();
+		tweens.contentEntranceSetup();
 	};
 
 	loadMedia = () => {
 		textBoxes = [];
-		tweensPosition = [];
-		tweensRotation = [];
 		boxSize = 6;
 		textBoxes = createContentBlocks(media, maxAnisotropy, 500, 500);
 
@@ -160,11 +267,8 @@ export default class ThreeDeeLogo extends Component {
 			mediaBox.translateY(everyOtherIndex * -boxSize);
 			mediaBox.rotateX(everyOtherIndex * 0.2);
 			mediaBox.translateX(15 * isEven);
-			tweensPosition.push(new TWEEN.Tween(mediaBox.position));
-			tweensRotation.push(new TWEEN.Tween(mediaBox.rotation));
 		});
-		console.log(tweensRotation);
-		console.log(textBoxes);
+		tweens.resetTweens();
 	};
 
 	loadLogo = () =>
@@ -208,91 +312,6 @@ export default class ThreeDeeLogo extends Component {
 			.repeat(Infinity)
 			.yoyo(true)
 			.start();
-	};
-
-	contentEntranceAnimation = () => {
-		animationRunning = true;
-		tweensPosition.forEach(tween => {
-			tween
-				.to({ y: "+7" }, 1000)
-				.easing(TWEEN.Easing.Quartic.Out)
-				.delay(1000)
-				.start();
-		});
-		tweenLogoPosition
-			.to({ y: 1.2 }, 1000)
-			.easing(TWEEN.Easing.Quartic.Out)
-			.delay(1000)
-			.start()
-			.onComplete(() => (animationRunning = false));
-
-		tweensPosition = [];
-		textBoxes.forEach(textBox => {
-			tweensPosition.push(new TWEEN.Tween(textBox.position));
-		});
-		tweenLogoPosition = new TWEEN.Tween(logo.position);
-	};
-
-	contentExitAnimation = () => {
-		animationRunning = true;
-		tweensPosition.forEach(tween => {
-			tween
-				.to({ y: "+30" }, 1000)
-				.easing(TWEEN.Easing.Quartic.Out)
-				.start()
-				.onComplete(() => (animationRunning = false));
-		});
-
-		tweenLogoPosition
-			.to({ y: 2.2 }, 500)
-			.easing(TWEEN.Easing.Quartic.Out)
-			.start();
-		tweenLogoPosition = new TWEEN.Tween(logo.position);
-	};
-
-	mediaEntranceAnimation = () => {
-		animationRunning = true;
-		tweensPosition.forEach((tween, i) => {
-			const isEven = i % 2 === 0 ? -1 : 1;
-			const x = isEven * 3;
-			tween
-				.to({ x }, 1000)
-				.easing(TWEEN.Easing.Quartic.Out)
-				.delay(1000)
-				.start()
-				.onComplete(() => (animationRunning = false));
-		});
-
-		tweenLogoPosition
-			.to({ y: 1.2 }, 1000)
-			.easing(TWEEN.Easing.Quartic.Out)
-			.delay(1000)
-			.start();
-
-		tweensPosition = [];
-		textBoxes.forEach(textBox => {
-			tweensPosition.push(new TWEEN.Tween(textBox.position));
-		});
-		tweenLogoPosition = new TWEEN.Tween(logo.position);
-	};
-
-	mediaExitAnimation = () => {
-		animationRunning = true;
-		tweensPosition.forEach((tween, i) => {
-			const isEven = i % 2 === 0 ? -1 : 1;
-			const x = isEven * 15;
-			tween
-				.to({ x }, 1000)
-				.easing(TWEEN.Easing.Quartic.Out)
-				.start()
-				.onComplete(() => (animationRunning = false));
-		});
-
-		tweenLogoPosition
-			.to({ y: 2.2 }, 500)
-			.easing(TWEEN.Easing.Quartic.Out)
-			.start();
-		tweenLogoPosition = new TWEEN.Tween(logo.position);
 	};
 
 	animate = () => {
